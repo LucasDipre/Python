12e8710d1b3ef7e1f348533fd76ff4c90fc19ce3
Lucas Dipré <lucas.dipre@gmail.com>
2023-07-31 01:31:15 -0300

    Modulo 8 - Import de arquivos e chamadas de funções, no jogo da adivinhacao e forca

---
+++ Python_ComecandoComALinguagem/Desafios_Exemplos_Curso/importacao_diferente/principal.py
@@ -0,0 +1,12 @@
+# principal.py
+
+import a
+import b
+
+#Como nos arquivos a.py e b.py temos somente funções elas precisam ser chamadas aqui para
+#serem executadas
+
+#mas se fizermos
+
+a.executa()
+b.executa()
